820e7e5e12a8508d874e2bf18fc41fbc8a2f4197
Riteek Rakesh <riteek7.12@gmail.com>
2024-08-27 09:33:14 +0530

    made code more optimised for large files

--- index.js
+++ index.js
@@ -1,65 +1,100 @@
-const fs = require('fs')
+const fs = require('fs');
 const WebSocket = require('ws');
-const path = require('path')
-const http = require('http')
-const express = require('express')
+const path = require('path');
+const http = require('http');
+const express = require('express');
 
 const LOG_PATH_FILE = path.join(__dirname, 'logfile.log'); // Path to your log file.
-const PORT = 8080;
 const HTTP_PORT = 3000;
 
-const app = express()
-const server = http.createServer(app)
-const wss = new WebSocket.Server({server})
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 let clients = [];
 let lastSize = 0;
 
+// Function to get the last N lines of a file
+function getLastNLines(filepath, n, callback) {
+    const stream = fs.createReadStream(filepath, {
+        encoding: 'utf8',
+        highWaterMark: 1024 * 64 // Read in chunks of 64KB
+    });
+
+    let data = '';
+    let lines = [];
+    
+    stream.on('data', chunk => {
+        data = chunk + data;
+        lines = data.split('\n');
+        if (lines.length > n + 1) {
+            stream.destroy(); // Stop reading once we have enough lines
+            callback(null, lines.slice(-n).join('\n'));
+        }
+    });
+
+    stream.on('end', () => {
+        if (lines.length <= n) {
+            callback(null, lines.join('\n'));
+        }
+    });
+
+    stream.on('error', err => {
+        callback(err, null);
+    });
+}
+
 // Serve the HTML file
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send("Go to /log endpoint");
+});
+
 app.get('/log', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
-// Websocket server
-fs.watchFile(LOG_PATH_FILE,(curr, prev)=>{
-    console.log("in watch function")
-    if(curr.size > lastSize){
+
+// WebSocket server setup
+fs.watchFile(LOG_PATH_FILE, (curr, prev) => {
+    if (curr.size > lastSize) {
         const stream = fs.createReadStream(LOG_PATH_FILE, {
-                start: lastSize,
-                end: curr.size,
-                encoding: 'utf-8'
+            start: lastSize,
+            end: curr.size,
+            encoding: 'utf8'
         });
 
         let data = '';
         stream.on('data', chunk => data += chunk);
-        stream.on('end', ()=>{
-            clients.forEach(client =>{
-                if (client.readyState === WebSocket.OPEN){
+        stream.on('end', () => {
+            clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
                     client.send(data);
                 }
-            })
-        })
+            });
+        });
+
         lastSize = curr.size;
     }
-})
-wss.on('connection', (ws)=>{
-    clients.push(ws)
+});
 
-    fs.readFile(LOG_PATH_FILE, 'utf-8', (err, data)=>{
-        if(err) throw err;
-        const lines = data.trim().split('\n');
-        const last10lines = lines.splice(-10).join('\n')
+wss.on('connection', (ws) => {
+    clients.push(ws);
 
-        ws.send(last10lines)
-    })
+    // Send the last 10 lines to the new client
+    getLastNLines(LOG_PATH_FILE, 10, (err, lastLines) => {
+        if (err) {
+            console.error('Error reading last lines:', err);
+        } else {
+            ws.send(lastLines);
+        }
+    });
 
-    ws.on('close', ()=>{
-        clients = clients.filter(client => client !== ws)
-    })
-})
+    ws.on('close', () => {
+        clients = clients.filter(client => client !== ws);
+    });
+});
 
-
-
-server.listen(HTTP_PORT, ()=>{
-    console.log(`HTTP server is runnning on http://localhost:${HTTP_PORT}`)
-})
+server.listen(HTTP_PORT, () => {
+    console.log(`HTTP server is running on http://localhost:${HTTP_PORT}`);
+});
